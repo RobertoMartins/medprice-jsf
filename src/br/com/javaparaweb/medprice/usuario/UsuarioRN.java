eb06746c21af956cfc66d6bdc221b07bb24ba86a
Roberto Murilo Martins Cordeiro <robertomartins@users.noreply.github.com>
2021-02-12 18:36:13 -0300

    ruim

--- src/br/com/javaparaweb/medprice/usuario/UsuarioRN.java
+++ src/br/com/javaparaweb/medprice/usuario/UsuarioRN.java
@@ -6,28 +6,31 @@ import java.util.List;
 
 public class UsuarioRN {
 	private UsuarioDAO usuarioDAO;
-	
+
 	public UsuarioRN() {
 		this.usuarioDAO = DAOFactory.criarUsuarioDAO();
 	}
 	
+	public Usuario carregar(Integer codigo) {
+		return this.usuarioDAO.carregar(codigo);
+	}
+	
 	public void salvar(Usuario usuario) {
 		Integer idUsuario = usuario.getIdUsuario();
+		System.out.println(usuario.getIdUsuario());
 		if (idUsuario == null || idUsuario == 0) {
-			usuario.getPermissao().add("ROLE_USUARIO");
 			this.usuarioDAO.salvar(usuario);
 		} else {
 			this.usuarioDAO.atualizar(usuario);
 		}
 	}
-	
+
 	public List<Usuario> listar() {
 		return this.usuarioDAO.listar();
 	}
-	
-	//public void excluir(Usuario usuario) {
-	//	this.usuarioDAO.excluir(usuario);
-	//}
-	
-	
+
+	// public void excluir(Usuario usuario) {
+	// this.usuarioDAO.excluir(usuario);
+	// }
+
 }
